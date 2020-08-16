198a682ce60634dd41188de95f0d501b3ff13b58
Andrei Kirillovykh <andykirill@gmail.com>
2020-08-16 15:24:03 +0300

    2. Парсинг HTML страницы. [#260358]

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,19 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            System.out.println(href.parent().parent().child(5).text());
+        }
+    }
+}
