16224b0a0608802c25493a4445c9b3ee7302220b
Andrei Kirillovykh <andykirill@gmail.com>
2020-08-17 11:16:35 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,6 +5,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
@@ -13,7 +17,35 @@ public class SqlRuParse {
             Element href = td.child(0);
             System.out.println(href.attr("href"));
             System.out.println(href.text());
-            System.out.println(href.parent().parent().child(5).text());
+            System.out.println(getDate(href.parent().parent().child(5).text()));
         }
+    }
+
+    public static Date getDate(String dateString) {
+        Calendar c = new GregorianCalendar();
+        String[] monthes = {"янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"};
+        String[] dateMass = dateString.replace(",", "").split(" ");
+        if (dateMass[0].contains("вчера")) {
+            c.set(Calendar.DAY_OF_MONTH, c.get(Calendar.DAY_OF_MONTH) - 1);
+            c.set(Calendar.HOUR, Integer.parseInt(dateMass[1].split(":")[0]));
+            c.set(Calendar.MINUTE, Integer.parseInt(dateMass[1].split(":")[1]));
+        } else if (dateMass[0].contains("сегодня")) {
+            c.set(Calendar.HOUR, Integer.parseInt(dateMass[1].split(":")[0]));
+            c.set(Calendar.MINUTE, Integer.parseInt(dateMass[1].split(":")[1]));
+        } else {
+            for (int i = 0; i < monthes.length; i++) {
+                if (monthes[i].equals(dateMass[1])) {
+                    dateMass[1] = String.valueOf(i);
+                }
+            }
+            c.set(
+                    Integer.parseInt("20" + dateMass[2]),
+                    Integer.parseInt(dateMass[1]),
+                    Integer.parseInt(dateMass[0]),
+                    Integer.parseInt(dateMass[3].split(":")[0]),
+                    Integer.parseInt(dateMass[3].split(":")[1])
+            );
+        }
+        return c.getTime();
     }
 }
